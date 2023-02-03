21b1426ce1c3a29758ed16d61e82dcdbd31b933b
IIMRLII <1104052058@qq.com>
2023-02-03 13:19:46 +0800

    gzip

--- config/index.js
+++ config/index.js
@@ -19,6 +19,19 @@ module.exports = {
   //           symbolId: 'icon-[name]'
   //       });
   // },
+  chainWebpack: config => {
+    const CompressionWebpackPlugin = require('compression-webpack-plugin')
+    if (process.env.NODE_ENV === 'production') {
+        config.plugin('CompressionPlugin').use(
+        	new CompressionWebpackPlugin({
+            test: /\.(js|css)$/,
+            threshold: 10240, // 超过10kb的文件就压缩
+      			deleteOriginalAssets:true, // 不删除源文件
+      			minRatio: 0.8
+          })
+        )
+   	}
+  },
   dev: {
 
     // Paths
